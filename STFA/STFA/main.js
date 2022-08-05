b90990823fb0636a7d8091fb518fde5a570cb00c
melquidesec <parawoww@gmail.com>
2022-08-05 16:10:30 -0500

    agregada primera version

---
+++ STFA/STFA/main.js
@@ -0,0 +1,16 @@
+const nombre = document.getElementById('nombre')
+const telefono = document.getElementById('telefono')
+const contraseña = document.getElementById('contraseña')
+const verificacion = document.getElementById('verificacion')
+
+button.addEventListener('click', (e) => {
+    e.preventDefault()
+    const data = {
+        nombre: nombre.value,
+        contraseña: contraseña.value,
+        telefono: telefono.value,
+        verificacion: verificacion.value,
+    }
+
+    console.log(data)
+})
